a93bf6533586a87fa31ee1d99030b20ec942503d
DongJu Kim <gggg8657@gmail.com>
2023-04-18 16:45:07 +0900

    0418_example

---
+++ 2023_1st_coding_test/addtoTOtal.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int total(int input){
+    static int tmp;
+    tmp = tmp + input;
+    return tmp;
+}
+
+int main(){
+    int iter = 0;
+    int inp;
+    while(iter <3){
+        scanf("%d", &inp);
+        printf("the result of sumation is :: %d\n",total(inp));
+
+    }
+}
