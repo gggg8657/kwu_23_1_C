2e22b3151e1e8824ef77086422cd6e5b80c8c168
DongJu Kim <gggg8657@gmail.com>
2023-05-25 12:20:25 +0900

    kwu C programming 0525 pointer example solution

--- source/2022110603_pointer.c
+++ source/2022110603_pointer.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
+void swap(int *p1, int *p2);
+
 int main()
 {
-    int a, b, tmp;
+    int a, b;
     int *p1, *p2;
     p1 = &a;
     p2 = &b;
@@ -11,10 +13,16 @@ int main()
     printf("value_B값 입력 : ");
     scanf("%d", &b);
 
-    tmp = *p1;
-    *p1 = *p2;
-    *p2 = tmp;
+    swap(p1,p2);
+
     printf("changed value of variable of A is %d \nchanged value of variable of B is %d \n", *p1, *p2);
     printf("\n\n*\t*\t*\t*end of the program*\t*\t*\t*\n");
     return 0;
+}
+
+void swap(int *p1, int *p2){
+    int tmp;
+    tmp = *p1;
+    *p1 = *p2;
+    *p2 = tmp;
 }
